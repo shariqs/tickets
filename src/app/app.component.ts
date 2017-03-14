230f71648ab74fdb5192f0d75cc7d9e22eb1da52
Shariq Shah <Shariq Shah>
2017-03-14 00:01:03 -0700

    Added Angularfire2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthMethods, AuthProviders } from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
+
+export class AppComponent {
+  auth: any;  // Instance of the User coming from app.component.
+
+
+  // Saves angularFire object as variable "af" that can be used anywhere in the component
+  constructor(public af: AngularFire) {
+
+    /**
+     * You can subscribe to certain variables.
+     * What this does is that when this variable is changed in any way, it runs the following callback (bit of code).
+     */
+    this.af.auth.subscribe(authenticationValue => {
+      this.auth = authenticationValue;
+    });
+  }
+
+  /**
+   * Logs in through Google.
+   */
+  private loginGoogle() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    });
+  }
+
+// Logout button
+  private logout() {
+    this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
 import { MenuBarComponent } from './menu-bar.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBWGRvdZwAd7x0-zvCjo4R9wUvJXY-sNQA',
+    authDomain: 'ticketsheister.firebaseapp.com',
+    databaseURL: 'https://ticketsheister.firebaseio.com',
+    storageBucket: 'ticketsheister.appspot.com',
+    messagingSenderId: '596010525706'
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -15,7 +30,8 @@ import { MenuBarComponent } from './menu-bar.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig,  myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
