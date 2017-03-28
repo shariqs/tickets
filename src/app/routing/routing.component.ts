667f2d164a7cdd08f4ab8fa60728a97d1258961e
techmciver <Public@unknown.attlocal.net>
2017-03-27 21:12:00 -0700

    fixed get website error by changing path of the routing component for body component

--- src/app/routing/routing.component.ts
+++ src/app/routing/routing.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
-import { BodyComponent} from '../body.component';
+import { BodyComponent} from '../body/body.component';
 import { MenuBarComponent } from '../menu-bar/menu-bar.component';
 import { MainMenuComponent } from '../main-menu/main-menu.component';
 import { AppComponent } from '../app.component';
