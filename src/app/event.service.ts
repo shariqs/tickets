07181435c1a725e5101184e56225ef0448a8fdd9
techmciver <Public@unknown.attlocal.net>
2017-04-02 19:31:04 -0700

    console log

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -12,11 +12,14 @@ public eventList = [];
   constructor(public http: Http) {   this.getEventFromLocalArea();  }
 
 
+
+
 getEventFromLocalArea(){
       var eventData: any = this.http.get('http://api.songkick.com/api/3.0/events.json?location=geo:37.3382,-121.8863&apikey=147UvqDDrnGJk7nh');
       eventData.subscribe(info => { 
             let response = JSON.parse(info._body);
             this.eventList = (response.resultsPage.results.event);
+            console.log(this.eventList)
             
       })
 
