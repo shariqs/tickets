789f1df2d6f1fb743cead947ff39c4811fba2406
Cody Melthratter <cody.melthratter@sjsu.edu>
2017-04-12 22:16:28 -0700

    commit.txt

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -11,7 +11,7 @@ import { EventService } from '../event.service';
 export class MenuBarComponent {
     constructor(public eventService: EventService, private router: Router) { }
 
-    menuItems = ['Search', 'Browse', 'Sell', 'Transactions'];
+    menuItems = ['Browse', 'Transactions'];
     
     onClick(){
       this.eventService.transactionInProgress = 'browse';
