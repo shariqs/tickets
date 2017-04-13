5069ed13e31fc67b193715a40baf16eb53bddc93
Melthratter <codyx.melthratter@intel.com>
2017-04-13 13:53:28 -0700

    src/commit.txt

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -71,7 +71,80 @@ class Event {
 
             return "Event Name: " + this.displayName + ";" + 
                    "Venue: "      + this.venue + ";" +
-                   "Date & Time: "       + this.time + ", " + this.date + ";" + 
+                   "Time & Date: "       + this.time + ", " + this.getMonth(this.date) + " " + this.getDay(this.date) + ", " + this.getYear(this.date) + ";" + 
                    "City: "       + this.city
+      }
+      getYear(date: string) : string {
+            return date.substring(0, 4);
+      }
+
+      getDay(date: string) : string {
+            var day = date.substring(8);
+            var dayInt = parseInt(day);
+            if (dayInt == 1) {
+                  day = day.concat("st");
+            } else if (dayInt == 2) {
+                  day = day.concat("nd");
+            } else if (dayInt == 3) {
+                  day = day.concat("rd");
+            } else {
+                  day = day.concat("th");
+            }
+            return day;
+      }
+
+      getMonth(date: string) : string {
+            var month = date.substring(5, 7);
+            switch (month) {
+                  case '01' :{
+                        month = "January";
+                        break;
+                  }
+                  case '02' :{
+                        month = "February";
+                        break;
+                  }
+                  case '03' :{
+                        month = "March";
+                        break;
+                  }
+                  case '04' :{
+                        month = "April";
+                        break;
+                  }
+                  case '05' :{
+                        month = "May";
+                        break;
+                  }
+                  case '06' :{
+                        month = "June";
+                        break;
+                  }
+                  case '07' :{
+                        month = "July";
+                        break;
+                  }
+                  case '08' :{
+                        month = "August";
+                        break;
+                  }
+                  case '09' :{
+                        month = "September";
+                        break;
+                  }
+                  case '10' :{
+                        month = "October";
+                        break;
+                  }
+                  case '11' :{
+                        month = "November";
+                        break;
+                  }
+                  case '12' :{
+                        month = "December";
+                        break;
+                  }
+            }
+      return month;
       }
 }
