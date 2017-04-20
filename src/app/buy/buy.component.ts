171fe87ddfa8b507c7a1c9b0ca91606f8b9c6a54
Hung Tran <sis.viethung@gmail.com>
2017-04-20 13:58:16 -0700

    Trying to fix sell component

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -10,29 +10,37 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
   templateUrl: './buy.component.html',
   styleUrls: ['./buy.component.css']
 })
-export class BuyComponent{
+export class BuyComponent implements OnInit{
 
 
   availableListings : Event[] = [];
 
-  constructor(public eventService: EventService, private popup:Popup, private router: Router, private route: ActivatedRoute, public af : AngularFire) { }
+  constructor(
+    public eventService: EventService, 
+    private popup:Popup,
+    private router: Router,
+    private route: ActivatedRoute, 
+    public af : AngularFire) { }
+
     private sub: any;
     private parentRouteId: number;
     private id: any;
     private idNum: number;
     private uid;
+    
   ngOnInit() {
 
     this.af.auth.subscribe(x => {
       this.uid = x.uid;
     })
-      this.sub = this.route.parent.params.subscribe(params => {
+    
+    this.sub = this.route.parent.params.subscribe(params => {
       this.id = +params["id"];
-      console.log("this is the id (buy form):"+this.id);
+      console.log("this is the id (sell form):"+this.id);
       this.idNum = this.id;
     });
+
     this.eventService.activeEventData
-    this.populateArray();
   }
   
   private ngDoCheck(){
@@ -63,12 +71,6 @@ export class BuyComponent{
 
 
   //BAD CODE THIS NEEDS TO BE FIXED LATER THERE MUST BE SOME WAY TO LISTEN FOR CHANGES ON ACTIVEEVENT!
-  populateArray(){
-    //console.log(this.eventService.activeEventData.id + " FROM BUYCOMPONENT");
-    this.af.database.list('Active_Listings/'+ this.eventService.activeEventData.id +'/').subscribe(allListings => {
-      this.availableListings = allListings;
-    });
-  }
 
 
   private ngOnDestroy() {
@@ -76,25 +78,4 @@ export class BuyComponent{
   }
 
 
-//THIS SHOULD NOT BE HERE IT SHOULD BE IN DATASERVICE BUT I HAVE NO ENERGY
-  buyTicket(purchased){
-    //Removes Listing from Active_Listings
-    this.af.database.object('Active_Listings/'+ this.eventService.activeEventData.id +'/' + purchased.$key).remove();
-    //Adds Listing to Completed_Transactions
-    var info = this.af.database.list('Completed_Transactions/'+ this.eventService.activeEventData.id).push(purchased);
-    //Adds Listing to Purchased for specific user
-    this.af.database.list('Users/' + this.uid + '/Purchased/' + this.eventService.activeEventData.id).push(info.key);
-    //Removes Listing from User's Active_Listing
-    this.af.database.list('Users/' + this.uid + '/Active_Listings/' + this.eventService.activeEventData.id).subscribe(listings => {
-      listings.forEach(listing => {
-        if(listing.$value == purchased.$key){
-          this.af.database.list('Users/' + this.uid + '/Active_Listings/' + this.eventService.activeEventData.id + "/"  + listing.$key).remove();
-        }
-      });
-    });
-    //Adds Listing to User's Sold
-    this.af.database.list('/Users/' + this.uid + '/Sold/' + this.eventService.activeEventData.id).push(info.key);
-    alert('sold');
-
-  }
 }
